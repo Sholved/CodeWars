3a96d88bd34814312ab0c63ecf71cbd517d12042
Sholved <dianemaggie0@gmail.com>
2024-02-17 14:30:00 +0000

    We continue

--- Login_auth/Login/models.py
+++ Login_auth/Login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user = get_user_model()
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True)
+    date = models.DateTimeField
