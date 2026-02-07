2a5f6ab39e32d8bb9efa8518beb49957839e4ce0
Sholved <dianemaggie0@gmail.com>
2026-02-07 01:48:05 +0000

    correcting the errors

--- duplicates.java
+++ duplicates.java
@@ -4,10 +4,11 @@ public class duplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("please enter a word");
-        String word = sc.nextLine();
+        String word = sc.nextLine().toLowerCase();
         int length = word.length();
         String[] letters = new String[length];
         int times = 0;
+        int index = 0;
 
         for (int i = 0; i <length; i ++){
             char c = word.charAt(i);
@@ -25,3 +26,4 @@ public class duplicates {
         sc.close();
     }
 }
+ 
