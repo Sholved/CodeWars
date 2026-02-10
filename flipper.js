86a282127d1e479ff8bd58a92a00b47d5df8f793
Sholved <dianemaggie0@gmail.com>
2026-02-10 10:30:00 +0000

    the beginning

--- flipper.js
+++ flipper.js
@@ -1,2 +1,27 @@
-let name = "Margaret";
-let age = 22;
+const colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "purple",
+    "orange",
+    "pink",
+    "cyan",
+    "lime",
+    "violet"
+]
+//event listeners
+const label = document.getElementById("colorlabel");
+const button = document.getElementById("fipbutton");
+
+// function to pick random colors
+function flipcolor(){
+    // pick a random position from the array
+    const randomindex = Math.floor(Math.random() * colors.length);
+    // get the value at that position
+    const color = colors[randomindex];
+    // change background
+    document.body.style.backgroundColor = color;
+    // change text label
+    label.textContent = "Current color: " + color;
+}
