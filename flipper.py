ba9fd2bb4461766a49c6d29694312aec854b76d5
Sholved <dianemaggie0@gmail.com>
2026-02-12 22:50:12 +0000

    on it

--- flipper.py
+++ flipper.py
@@ -1,4 +1,12 @@
 import tkinter as tk
 import random
 
-colors[] = ["green", "pink", "red", "white", "blue", "green"]
+root = tk.Tk()
+root.title("My color flipper")
+root.geometry("400x300")
+# Create a label for your window
+label = tk.Label(root, test = "Click the button to flip color", font=("Arial", 16))
+label.pack(pady = 30) #pack places the label in the window and paddy adds vertical space
+
+root.mainloop
+# colors[] = ["green", "pink", "red", "white", "blue", "green"]
