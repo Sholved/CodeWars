473d9164754ee1824a838c6c673de57f6e7a9829
Sholved <dianemaggie0@gmail.com>
2026-02-12 23:21:35 +0000

    ..

--- flipper.py
+++ flipper.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("My color flipper")
 root.geometry("400x300")
 # Create a label for your window
-label = tk.Label(root, test = "Click the button to flip color", font=("Arial", 16))
+label = tk.Label(root, text = "Click the button to flip color", font=("Arial", 16))
 label.pack(pady = 30) #pack places the label in the window and paddy adds vertical space
 
 root.mainloop
