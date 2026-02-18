783f2f96b698fe6f2719b6cf24deeeac6856471c
Sholved <dianemaggie0@gmail.com>
2026-02-18 22:12:02 +0000

    still continuing

--- Login_auth/Login/models.py
+++ Login_auth/Login/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-user = get_user_model()
+User = get_user_model()
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField(null=True)
-    date = models.DateTimeField
-    location = models.TextField(null = True)
-    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    date = models.DateTimeField()
+    location = models.TextField(blank = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    
+    def __str__(self):
+        return self.title
